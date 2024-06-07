7a6171b468cdfb4d58e3bcf1a3a13fa8d4a2b880
AmberParmaar <160378243+Amberparmaar@users.noreply.github.com>
2024-06-08 01:41:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const quiz = await inquirer.prompt([
+    {
+        name: "question_1",
+        type: "list",
+        message: "Which is the capital city of Pakistan?",
+        choices: ["Lahore", "Faisalabad", "Karachi", "Islamabad"]
+    },
+    {
+        name: "question_2",
+        type: "list",
+        message: "Who is the founder of Pakistan?",
+        choices: ["Quaid e Azam", "Allama Iqbal", "Sir Syed", "Fatima Jinnah"]
+    },
+    {
+        name: "question_3",
+        type: "list",
+        message: "Which city is called city of light?",
+        choices: ["Multan", "Faisalabad", "Karachi", "Sialkot"]
+    },
+    {
+        name: "question_4",
+        type: "list",
+        message: "Who inaugurated the State Bank of Pakistan?",
+        choices: ["Quaid-e-Azam", "Malik Ghulam Muhammad", "Liaquat Ali Khan", "Allama Muhammad Iqbal"]
+    },
+    {
+        name: "question_5",
+        type: "list",
+        message: "National code of Pakistan is?",
+        choices: ["PAK", "PAK1", "PK", "Non of them"]
+    }
+]);
+let score = 0;
+switch (quiz.question_1) {
+    case "Islamabad":
+        console.log(`1. Correct`);
+        ++score;
+        break;
+    default:
+        console.log(`1. Incorrect`);
+}
+switch (quiz.question_2) {
+    case "Quaid e Azam":
+        console.log(`1. Correct`);
+        ++score;
+        break;
+    default:
+        console.log(`1. Incorrect`);
+}
+switch (quiz.question_3) {
+    case "Karachi":
+        console.log(`1. Correct`);
+        ++score;
+        break;
+    default:
+        console.log(`1. Incorrect`);
+}
+switch (quiz.question_4) {
+    case "Quaid e Azam":
+        console.log(`1. Correct`);
+        ++score;
+        break;
+    default:
+        console.log(`1. Incorrect`);
+}
+switch (quiz.question_5) {
+    case "PK":
+        console.log(`1. Correct`);
+        ++score;
+        break;
+    default:
+        console.log(`1. Incorrect`);
+}
+console.log(`score: ${score}`);
